82409ce2b5348d912ecac2a0a6dabf4553d4b091
unknown <dusanthansivarajah@hotmail.com>
2024-06-30 22:53:37 +0300

    added mouse hover and audio

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,25 @@ const red = document.getElementById("red");
 const yellow = document.getElementById("yellow");
 const blue = document.getElementById("blue");
 const green = document.getElementById("green");
+const hover= document.querySelector(".btn");
+
+const redSound = new Audio('assets/sounds/red.mp3');
+const blueSound = new Audio('assets/sounds/blue.mp3');
+const greenSound = new Audio('assets/sounds/green.mp3');
+const yellowSound = new Audio('assets/sounds/yellow.mp3');
+const wrongSound = new Audio('assets/sounds/wrong.mp3');
+
+
+
+red.addEventListener("mouseenter",()=> red.classList.add("pressed") );
+red.addEventListener("mouseleave",()=> red.classList.remove("pressed") );
+blue.addEventListener("mouseenter",()=> blue.classList.add("pressed") );
+blue.addEventListener("mouseleave",()=> blue.classList.remove("pressed") );
+green.addEventListener("mouseenter",()=> green.classList.add("pressed") );
+green.addEventListener("mouseleave",()=> green.classList.remove("pressed") );
+yellow.addEventListener("mouseenter",()=> yellow.classList.add("pressed") );
+yellow.addEventListener("mouseleave",()=> yellow.classList.remove("pressed") );
+
 
 
 const buttonColors = [red, blue, green, yellow];
@@ -12,25 +31,34 @@ let started = false;
 let level = 0;
 
 
+const buttonSounds = {
+    red: redSound,
+    blue: blueSound,
+    green: greenSound,
+    yellow: yellowSound
+  };
+
 
 function getRandomColor(color){
     let randomNumber= Math.floor(Math.random()*4);
     return color[randomNumber]
 }
 function nextColor(){
-    let colorSelect=getRandomColor(buttonColors)
+    let colorSelect=getRandomColor(buttonColors);
     colorSelect.classList.add("pressed"); 
-    gamePattern.push(colorSelect)
+    gamePattern.push(colorSelect);
+    buttonSounds[colorSelect.id].play();
     setTimeout(function(){
     colorSelect.classList.remove("pressed")},100);   
-    console.log(gamePattern)
+    console.log(gamePattern);
     level++;
-    document.querySelector("#level-title").textContent=`Level ${level}`
+    document.querySelector("#level-title").textContent=`Level ${level}`;
 }
 
 function checkClicked(color){
-    userClickedPattern.push(color)
-    console.log(userClickedPattern)
+    userClickedPattern.push(color);
+    buttonSounds[color.id].play();
+    console.log(userClickedPattern);
     patternTest(userClickedPattern.length -1);
 }
 
@@ -48,6 +76,7 @@ function patternTest(currentLevel){
 
     }else{
         bg.classList.add("game-over");
+        wrongSound.play()
         document.querySelector("#level-title").textContent="Game Over, Press Any Key to Restart";
         setTimeout(()=> bg.classList.remove("game-over"),200);
         restartGame();
@@ -74,6 +103,10 @@ document.addEventListener('keypress', () => {
     }
 });
 
+
+
+
+
 red.addEventListener("click",()=>{
     checkClicked(red)
  })
@@ -90,15 +123,5 @@ red.addEventListener("click",()=>{
 
 
 
-
-
-
-// now that the clicks been set, and shown in the array
-// i need to compare the clicks with the array 
-
-// if it matches with the array then continue to look for 
-// thee next color 
-
-// if it doesnt match then change the background red and reset
 
 
